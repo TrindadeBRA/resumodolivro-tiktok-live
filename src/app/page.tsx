c41caecfd1ef373aaaba7da5caa4a77192d91d5c
Lucas Trindade <trindadebra@gmail.com>
2025-03-02 04:09:34 -0300

    fix: cut messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-grea">
             <h1 className="text-3xl font-bold mb-6 text-center">Chat ao vivo do TikTok</h1>
             <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-6 h-[80vh] overflow-y-auto flex flex-col pb-10">
-                <div className="h-full overflow-y-auto flex flex-col pb-10">
+                <div className="h-full overflow-y-scroll flex flex-col">
                     {messages.map((msg, index) => ( // Renderizar todas as mensagens
                         <div key={index} className='flex flex-row items-start gap-3 mb-4 p-2 border-b border-gray-200'>
                             {msg.profilePictureUrl && ( // Verificar se a imagem de perfil existe
